703d54e1b7ba17bcd88370f21f84dce338cfe423
Alexander Pruss <arpruss@gmail.com>
2026-01-22 15:10:42 -0600

    code cleanup

--- verify.py
+++ verify.py
@@ -1,25 +1,38 @@
 import math
-import numpy as np
 import random
 import numbers
 from mpmath import mp
 
-import numpy as np
 
-count = 10000
-    
+# license: MIT
+
+#
+# No attempts at efficiency are made in this code. The main point is to ensure readability
+# for the sake of verification of formulae.
+#
+
+
+#mode = "fullyRandom" 
+#mode = "onPlane"
+mode = "cohorizontal"
+mth = math
+
+count = int(1e4)
+seed = 1
+eps = 1e-10
+switchToMP = 1e-5
 
 mp.dps = 20
 mp.prec = 150
 
-class myarray:
+class array:
     def __init__(self,a):
         self.data = []
         for row in a:
             if not hasattr(row, '__len__'):
                 self.data.append(row)
             else:
-                self.data.append(myarray(row))
+                self.data.append(array(row))
     
     def __getitem__(self, i):
         return self.data[i]
@@ -31,19 +44,19 @@ class myarray:
         return len(self.data)
 
     def __add__(self, a):
-        x = myarray(self.data)
+        x = array(self.data)
         for i in range(len(x)):
             x[i] += a[i]
         return x
         
     def __sub__(self, a):
-        x = myarray(self.data)
+        x = array(self.data)
         for i in range(len(x)):
             x[i] -= a[i]
         return x
         
     def __mul__(self, a):
-        x = myarray(self.data)
+        x = array(self.data)
         for i in range(len(x)):
             if hasattr(a,'__len__'):
                 x[i] *= a[i]
@@ -52,7 +65,7 @@ class myarray:
         return x
         
     def __truediv__(self, a):
-        x = myarray(self.data)
+        x = array(self.data)
         for i in range(len(self.data)):
             if hasattr(a,'__len__'):
                 x[i] /= a[i]
@@ -77,33 +90,40 @@ class myarray:
         for b in self:
             out += str(b) + " "
         return out+"]"
-
-array = myarray
-
-def norm(v):
-    s2 = 0
-    for a in v:
-        s2 += a*a
-    return mth.sqrt(s2)
-    
-def cross(a,b):
-    return array( ( a[1]*b[2]-a[2]*b[1], a[2]*b[0]-a[0]*b[2], a[0]*b[1]-a[1]*b[0] ) )
+        
+    def mpf(self):
+        out = []
+        for x in self:
+            if hasattr(x,'__len__'):
+                out.append(x.mpf())
+            else:
+                out.append(mp.mpmathify(x))
+        return array(out)
+        
+    def normSquared(self):
+        s2 = 0
+        for x in self:
+            if hasattr(x,'__len__'):
+                s2 += x.normSquared()
+            else:
+                s2 += x*x
+        return s2
+        
+    def norm(self):
+        return mth.sqrt(self.normSquared())
+        
+    def normalized(self):
+        return array(self / self.norm())
+        
+    def cross(self,b):
+        return array( ( self[1]*b[2]-self[2]*b[1], self[2]*b[0]-self[0]*b[2], self[0]*b[1]-self[1]*b[0] ) )
 
 # author: Alexander Pruss
-# license: MIT
-
-#
-# No attempts at efficiency are made in this code. The main point is to ensure readability
-# for the sake of verification of formulae.
-#
-
 # inputs: H = m23-m12 = altitude of landmark 2 above landmark 1
 #         d = horizontal distance between landmarks
 #         rho[0], rho[1] = observed tilt angles
 #         beta           = observed horizontal landmark angle from landmark 1 to 2
-#         eps            = tolerance for comparison with zero
 #
-# outputs: array of:
 #     (h1,d1,h2,d2)
 #         where: hi is altitude of camera above landmark i
 #                di is horizontal distance from landmark i to camera
@@ -114,9 +134,7 @@ def cross(a,b):
 def cot(x):
     return 1/mth.tan(x)
 
-def solve(H, d, rho, beta, eps=1e-12, assumeOnPlane=False):
-    scaledEps = eps * mth.hypot(H, d)
-    
+def solve(H, d, rho, beta, assumeOnPlane=False):
     if abs(rho[0]) <= eps or abs(rho[0]-mth.pi) <= eps:
         assert not( abs(rho[1]) < eps or abs(rho[1]-mth.pi) < eps )
         d1 = 0.
@@ -162,7 +180,7 @@ def solve(H, d, rho, beta, eps=1e-12, assumeOnPlane=False):
     
     discriminant = b**2 - 4 * a * c
     
-    if assumeOnPlane or abs(discriminant) <= scaledEps ** 2:
+    if assumeOnPlane or abs(discriminant) <= eps:
         dj = -b / (2 * a)
         solution = finishSolution(dj)
         if not solution:
@@ -170,7 +188,7 @@ def solve(H, d, rho, beta, eps=1e-12, assumeOnPlane=False):
         else:
             return [solution,]
             
-    assert discriminant > 0, "discriminant is "+str(discriminant)
+    assert discriminant > 0, "discriminant "+str(discriminant)
         
     solutions = []
     
@@ -186,10 +204,10 @@ def solve(H, d, rho, beta, eps=1e-12, assumeOnPlane=False):
 # 
 # find points at intersection of circles centered on A and B with radii rA and RB
 #
-def intersectionOfCircles(A,rA,B,rB,scaledEps):
+def intersectionOfCircles(A,rA,B,rB):
     diff = B-A
-    d = norm(diff)
-    if abs(rA-rB-d) <= scaledEps:
+    d = diff.norm()
+    if abs(rA-rB-d) <= eps:
         # one solution
         # return appropriate affine combination of points A and B
         return [ (A * rB + B * rA) / (rA+rB), ]
@@ -216,24 +234,22 @@ def intersectionOfCircles(A,rA,B,rB,scaledEps):
 #
 # algorithm: follow ideas in proof of Lemma 2
 
-def getCameraPosition(solution, beta, landmarks, eps=1e-15):
+def getCameraPosition(solution, beta, landmarks):
     h1,d1,h2,d2 = solution
     m = array([ landmarks[0], landmarks[1] ])
-    scale = norm( m[1]-m[0] )
-    scaledEps = scale * eps
-    if d1 <= scaledEps:
+    if d1 <= eps:
         return m[0] + [0,0,h1]
-    if d2 <= scaledEps:
+    if d2 <= eps:
         return m[1] + [0,0,h2]
     assert beta != 0
     A = array((m[0][0], m[0][1]))
     B = array((m[1][0], m[1][1]))
-    cc = intersectionOfCircles(A,d1,B,d2,scaledEps)
+    cc = intersectionOfCircles(A,d1,B,d2)
     for C in cc:
         # the sign of s is the same as that of the signed angle ACB
         AC = A-C
         BC = B-C
-        s = cross( (AC[0], AC[1], 0), (BC[0], BC[1], 0))[2] 
+        s = array((AC[0], AC[1], 0)). cross( (BC[0], BC[1], 0) )[2] 
         if len(cc) == 1 or (s>0) == (beta>0):
             # camera (x,y)=C and camera has altitude m[0]+[0,0,h1]
             return array((C[0],C[1],m[0][2]+h1))
@@ -250,32 +266,28 @@ def det3x3(a):
 # inputs: 
 #      camera:       position (x,y,z) of camera (z=altitude)
 #      landmarks[i]: position (x,y,z) of landmark i (z=altitude)
-#      eps:          tolerance for comparisons with zero
 # outputs: integer
 # algorithm: use characterization in Theorem 1
 # assuming landmarks are not at the same point
 #
 
-def howManySolutions(camera, landmarks, eps=1e-15):
+def howManySolutions(camera, landmarks, assumeOnPlane=False):
     # for convenience move camera to origin
     l = array( [ landmarks[0]-camera, landmarks[1]-camera ] )
-    scale = norm(l[1]-l[0])
-    assert scale != 0
-    scaledEps = scale * eps
     # type 1 degeneracy: camera and landmarks are colinear
-    if norm(cross( l[0], l[1] )) <= scaledEps ** 2:
+    if l[0].cross(l[1]).norm() <= eps:
         return mth.inf
     # type 2 degeneracy: landmarks are on same vertical line, but not type 1
     d = mth.hypot( l[0][0]-l[1][0], l[0][1]-l[1][1] )
-    if d <= scaledEps:
+    if d <= eps:
         return mth.inf
     # type 3 degeneracy: landmarks and camera on same horizontal plane, but not type 1
-    if abs(l[0][2]) <= scaledEps and abs(l[1][2]) <= scaledEps:
+    if abs(l[0][2]) <= eps and abs(l[1][2]) <= eps:
         return mth.inf
     # if max(|s1|,|s2|) >= |s|, one solution
     d1 = mth.hypot( l[0][0], l[0][1])
     d2 = mth.hypot( l[1][0], l[1][1])    
-    if d1 <= scaledEps or d2 <= scaledEps:
+    if d1 <= eps or d2 <= eps:
         # si would be infinite if di were zero
         return 1
     # slope from landmark 1 to landmark 2
@@ -289,12 +301,12 @@ def howManySolutions(camera, landmarks, eps=1e-15):
     if abs(s) > eps:
         # if camera lies on plane defined by landmarks and horizontal line through
         # landmark 1 and perpendicular to line through landmarks, have 1 solution
-        horizontalVector = cross(l[1]-l[0], array([0,0,1]))
-        p3 = l[0] + horizontalVector * scale
+        horizontalVector = (l[1]-l[0]).cross((0,0,1))
+        p3 = l[0] + horizontalVector
         # check if origin lies on plane defined by l0, l1, p3
         # i.e., check if the three vectors are linearly dependent
-        d = det( array( (p3,l[0],l[1]) ) )
-        if abs(d) <= scaledEps**3:
+        d = det3x3( (p3,l[0],l[1]) )
+        if assumeOnPlane or abs(d) <= eps:
             return 1
     
     return 2
@@ -309,12 +321,12 @@ def howManySolutions(camera, landmarks, eps=1e-15):
 def getObservations(camera, landmarks):
     # move camera to origin for simplicity
     m = ( landmarks[0] - camera, landmarks[1] - camera )
-    v1 = m[0] / norm(m[0])
-    v2 = m[1] / norm(m[1])
+    v1 = m[0].normalized()
+    v2 = m[1].normalized()
     rho = (mth.acos(v1[2]),mth.acos(v2[2])) 
     q1 = array((v1[0],v1[1],0.))
     q2 = array((v2[0],v2[1],0.))
-    beta = mth.atan2( cross( q1,q2 )[2], q1.dot(q2) )
+    beta = mth.atan2( q1.cross( q2 )[2], q1.dot(q2) )
     return rho,beta
     
 #
@@ -332,7 +344,7 @@ def getObservations(camera, landmarks):
 #                       simulated observations of the ground truth; Euclidean distance on the
 #                       triple (rho[0],rho[1],beta)
   
-def computeErrors(camera, landmarks, verbose=False, assumeOnPlane=False):
+def compute(camera, landmarks, verbose=False, assumeOnPlaneForSolutions=False, assumeOnPlaneForPredictions=False):
     H = landmarks[1][2]-landmarks[0][2]
     d = mth.hypot( landmarks[1][0]-landmarks[0][0], landmarks[1][1]-landmarks[0][1] )
     if verbose:
@@ -342,88 +354,111 @@ def computeErrors(camera, landmarks, verbose=False, assumeOnPlane=False):
         gh2 = camera[2]-landmarks[1][2]
         print("ground truth h1,d1,h2,d2: ",gh1,gd1,gh2,gd2)
     rho,beta = getObservations(camera, landmarks)
-    expectedSolutions = howManySolutions(camera,landmarks) 
+    expectedSolutions = howManySolutions(camera,landmarks, assumeOnPlane=assumeOnPlaneForPredictions) 
     if expectedSolutions == mth.inf:
         return None
-    solutions = solve(H,d,rho,beta,assumeOnPlane=assumeOnPlane)
+    solutions = solve(H,d,rho,beta,assumeOnPlane=assumeOnPlaneForSolutions)
     errPosition = mth.inf
     errObservation = 0
+    if verbose and len(solutions) != expectedSolutions:
+        print("Actual solutions: %d\nExpected solutions: %d" % (len(solutions),expectedSolutions))
     if not solutions:
-        return 0,mth.inf,0
+        return 0,mth.inf,0,0
     else:
         for solution in solutions:
             solvedC = getCameraPosition(solution, beta, landmarks)
             if verbose:
                 print(solution)
                 print(solvedC)
-            e = norm(solvedC-camera)
+            e = (solvedC-camera).norm()
             errPosition = min(e, errPosition)
             solvedRho,solvedBeta = getObservations(solvedC, landmarks)
-            e = norm(array((solvedRho[0],solvedRho[1],solvedBeta))-(rho[0],rho[1],beta))
+            e = (array((solvedRho[0],solvedRho[1],solvedBeta))-(rho[0],rho[1],beta)).norm()
             errObservation = max(e, errObservation)
-        return len(solutions)-expectedSolutions,errPosition,errObservation
+        return len(solutions)-expectedSolutions,errPosition,errObservation,len(solutions)
         
 def generateFullyRandom():
-    landmarks = ( array( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ), 
-                    array( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ) )
+    landmarks = array( ( ( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ), 
+                       ( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ) ) )
     camera = array( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) )
     return camera,landmarks
 
 def generateCameraOnPlane():
-    landmarks = ( array( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ), 
-                    array( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ) )
-    horiz = cross(landmarks[1]-landmarks[0], array([0,0,1]))
-    u = random.uniform(-5,5)
-    v = random.uniform(-5,5)
-    return landmarks[0] + (landmarks[1]-landmarks[0])*u + horiz*v,landmarks
+    landmarks = array( ( ( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ), 
+                    ( (random.uniform(-20,20),random.uniform(-20,20),random.uniform(-20,20)) ) ) )
+    joining = (landmarks[1]-landmarks[0]).normalized()
+    horiz = joining.cross((0,0,1))
+    u = random.uniform(-20,20)
+    v = random.uniform(-20,20)
+    return landmarks[0] + joining*u + horiz*v,landmarks
     
 def generateCameraOnPlane_mp():
     def r(x):
         return mp.mpmathify(random.uniform(-x,x))
-    landmarks = ( array( (r(20),r(20),r(20)) ), 
-                    array( (r(20),r(20),r(20)) ) )
-    horiz = cross(landmarks[1]-landmarks[0], array([0,0,1]))
-    u = r(5)
-    v = r(5)
-    return landmarks[0] + (landmarks[1]-landmarks[0])*u + horiz*v,landmarks
+    landmarks = array( ( (r(20),r(20),r(20)) ), 
+                    ( (r(20),r(20),r(20)) ) )
+    joining = (landmarks[1]-landmarks[0]).normalized()
+    horiz = joining.cross((0,0,1))
+    u = r(20)
+    v = r(20)
+    return landmarks[0] + joining*u + horiz*v,landmarks
     
-def generateWithCohorizonatalLandmarks():
+def generateWithCohorizontalLandmarks():
     camera, landmarks = generateFullyRandom()
-    return camera, (landmarks[0],(landmarks[1][0],landmarks[1][2],landmarks[0][2]))
+    return camera, array((landmarks[0],((landmarks[1][0],landmarks[1][2],landmarks[0][2]))))
     
-generator = generateWithCohorizonatalLandmarks
-mth = math
+if mode == "fullyRandom":
+    generator = generateFullyRandom
+    assumeOnPlaneForSolutions = False
+    assumeOnPlaneForPredictions = False
+elif mode == "onPlane":
+    generator = generateCameraOnPlane
+    assumeOnPlaneForSolutions = False
+    assumeOnPlaneForPredictions = True
+elif mode == "cohorizontal":
+    generator = generateWithCohorizontalLandmarks
+    assumeOnPlaneForSolutions = False
+    assumeOnPlaneForPredictions = False
 
 if __name__ == '__main__':
-    landmarks = array( ( (0,10,0), (4,10,3) ) )
-    random.seed(1)
+    random.seed(seed)
     errNumber = 0
     errPosition = 0
     errObservation = 0
     totalNumberError = 0
     totalPositionError = 0
     totalObservationError = 0
+    totalOneSolution = 0
+    switchedToMP = 0
     
     for i in range(count):
         camera,landmarks = generator()
         try:
-            n,p,o = computeErrors(camera,landmarks,assumeOnPlane=(generator==generateCameraOnPlane))
-        except:
-            p = 1
-        if p > 1e-5 and mth != mp:
+            n,p,o,sols = compute(camera,landmarks,assumeOnPlaneForPredictions=assumeOnPlaneForPredictions,assumeOnPlaneForSolutions=assumeOnPlaneForSolutions)
+        except AssertionError as e:
+            p = switchToMP*2
+        if n > 0:
+            p = switchToMP*2
+        if p > switchToMP and mth != mp:
             mth = mp
-            mpcamera = array([mp.mpmathify(c) for(c) in camera])
-            mplandmarks = array([array([mp.mpmathify(l) for(l) in landmark]) for landmark in landmarks])
-            n,p,o = computeErrors(mpcamera,mplandmarks,verbose=False,assumeOnPlane=(generator==generateCameraOnPlane))
-            #print("mperrors ",n,p,o)
+            mpcamera = camera.mpf()
+            mplandmarks = landmarks.mpf()
+            n,p,o,sols = compute(mpcamera,mplandmarks,verbose=False,assumeOnPlaneForPredictions=assumeOnPlaneForPredictions,assumeOnPlaneForSolutions=assumeOnPlaneForSolutions)
+#            if n != 0:
+#                compute(mpcamera,mplandmarks,verbose=True,assumeOnPlaneForPredictions=assumeOnPlaneForPredictions,assumeOnPlaneForSolutions=assumeOnPlaneForSolutions)
+            switchedToMP += 1
             mth = math
-        errNumber = max(n,errNumber)
-        totalNumberError += n
+        errNumber = max(errNumber,abs(n))
+        totalNumberError += abs(n)
         errPosition = max(p,errPosition)
         totalPositionError += p
         errObservation = max(o,errObservation)
         totalObservationError += o
+        if sols == 1:
+            totalOneSolution += 1
 
     print("n=",count)
+    print("one solution ratio=",totalOneSolution/count)
     print("Worst errors: %d %g %g" % (errNumber,errPosition,errObservation))
-    print("Average errors: %d %g %g" % (totalNumberError/count,totalPositionError/count,totalObservationError/count))
+    print("Average errors: %g %g %g" % (totalNumberError/count,totalPositionError/count,totalObservationError/count))
+    print("Ratio switched to MP: %g" % (switchedToMP/count))
